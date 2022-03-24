9c101b06adc991bd612f561d862cdbac058a8b15
LinZ1025 <90872950+LinZ1025@users.noreply.github.com>
2022-03-24 09:41:45 -0700

    updated F1

--- R/f1_point_of_interest.R
+++ R/f1_point_of_interest.R
@@ -1,8 +1,8 @@
-# Function 1: Your input for this function should be an individual year. 
+# Function 1: Your input for this function should be an individual year.
 # Your output should be a data point of interest in the dataset for some player.
 
 #' A nbaPlayers Function
-#' 
+#'
 #' This function allows you to calculate a data point of interest in the dataset for some player in any given year.
 #' @param data  The input dataset
 #' @param year The input value of year
@@ -16,7 +16,7 @@ data_point_of_interest <- function(data,year){
   library(dplyr)
   df <- subset(data, Year == year)
   df1 <- select(df, Year, Player,PTS)
-  df2 <- summarise(group_by(df1, Player), point_of_interest = sum(PTS))
+  df2 <- slice(summarise(group_by(df1, Player), point_of_interest = sum(PTS)),1)
   df2
 }
 
